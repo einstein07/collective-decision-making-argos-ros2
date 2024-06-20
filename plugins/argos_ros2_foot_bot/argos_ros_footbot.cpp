e7eddd5b3365d79184d95ae0c2d6fc98bb2fd2a3
einstein07 <mkhsin035@myuct.ac.za>
2024-06-20 13:18:53 +0200

    Initial commit

---
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.cpp
@@ -0,0 +1,11 @@
+/*
+ * argos_ros_footbot.cpp
+ *
+ *  Created on: 20 Jun 2024
+ *  Author: Sindiso Mkhatshwa
+ *  Email: sindiso.mkhatshwa@uni-konstanz.de
+ */
+
+
+
+
---
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.h
@@ -0,0 +1,24 @@
+/*
+ * argos_ros_footbot.h
+ *
+ *  Created on: 20 Jun 2024
+ *  Author: Sindiso Mkhatshwa
+ *  Email: sindiso.mkhatshwa@uni-konstanz.de
+
+*
+ */
+
+#ifndef ARGOS_ROS_FOOTBOT_H_
+#define ARGOS_ROS_FOOTBOT_H_
+
+#include <argos3/core/control_interface/ci_controller.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_colored_blob_omnidirectional_camera_sensor.h>
+
+
+
+
+
+
+#endif /* ARGOS_ROS_FOOTBOT_H_ */
