4327a530f5cc5d704bdefc7903d2a2412e823d31
einstein07 <mkhsin035@myuct.ac.za>
2024-07-08 11:43:01 +0200

    Fully functional argos-ros2 interface.

--- plugins/argos_ros2_foot_bot/argos_ros_footbot.cpp
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.cpp
@@ -6,6 +6,152 @@
  *  Email: sindiso.mkhatshwa@uni-konstanz.de
  */
 
+/* Include the controller definition */
+#include "argos_ros_footbot.h"
+using namespace std;
+using namespace argos_interface;
+using namespace argos_interface::msg;
+using std::placeholders::_1;
+
+/**
+ * Initialize the node before creating the publishers
+ * and subscribers. Otherwise we get a guard-error during
+ * compilation if we initialize the node after.
+ */
+std::shared_ptr<rclcpp::Node> initNode() {
+  int argc = 1;
+  char *argv = (char *) "";
+  rclcpp::init(argc, &argv);
+  
+  return std::make_shared<rclcpp::Node>("argos_ros_node");
+
+}
+
+std::shared_ptr<rclcpp::Node> ArgosRosFootbot::nodeHandle = initNode();
+
+ArgosRosFootbot::ArgosRosFootbot() :
+		m_pcWheels(NULL),
+		m_pcOmniCam(NULL),
+		m_pcProximity(NULL),
+		stopWithoutSubscriberCount(10),
+		stepsSinceCallback(0),
+		leftSpeed(0),
+		rightSpeed(0){}
+
+ArgosRosFootbot::~ArgosRosFootbot(){}
+
+void ArgosRosFootbot::Init(TConfigurationNode& t_node){
+	// Create the topics to publish
+	stringstream puckListTopic, proximityTopic;
+	puckListTopic << "/puck_list";
+	proximityTopic << "/proximity";
+	puckListPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<argos_interface::msg::PuckList>(puckListTopic.str(), 1);
+	promixityPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<argos_interface::msg::ProximityList>(proximityTopic.str(), 1);
+
+	// Create the subscriber
+	stringstream cmdVelTopic;
+	cmdVelTopic << "/cmd_vel";
+	cmdVelSubscriber_ = ArgosRosFootbot::nodeHandle -> create_subscription<geometry_msgs::msg::Twist>(
+			cmdVelTopic.str(),
+			1,
+			std::bind(&ArgosRosFootbot::cmdVelCallback, this, _1)
+			);
+
+	// Get sensor/actuator handles
+	m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+	m_pcProximity = GetSensor<CCI_FootBotProximitySensor>("footbot_proximity");
+	m_pcOmniCam = GetSensor<CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+
+	m_pcOmniCam->Enable();
+
+	/*
+	* Parse the configuration file
+	*
+	* The user defines this part. Here, the algorithm accepts three
+	* parameters and it's nice to put them in the config file so we don't
+	* have to recompile if we want to try other settings.
+	*/
+	GetNodeAttributeOrDefault(t_node, "stopWithoutSubscriberCount", stopWithoutSubscriberCount, stopWithoutSubscriberCount);
 
 
+}
 
+bool puckComparator(Puck a, Puck b) {
+	return a.angle < b.angle;
+}
+
+void ArgosRosFootbot::ControlStep() {
+	rclcpp::spin_some(ArgosRosFootbot::nodeHandle);
+	const CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings& camReads = m_pcOmniCam->GetReadings();
+	PuckList puckList;
+	puckList.n = camReads.BlobList.size();
+	for (size_t i = 0; i < puckList.n; ++i) {
+		Puck puck;
+		puck.type = (camReads.BlobList[i]->Color == CColor::RED);
+		puck.range = camReads.BlobList[i]->Distance;
+		// Make the angle of the puck in the range [-PI, PI].  This is useful for
+		// tasks such as homing in on a puck using a simple controller based on
+		// the sign of this angle.
+		puck.angle = camReads.BlobList[i]->Angle.SignedNormalize().GetValue();
+		puckList.pucks.push_back(puck);
+	}
+
+	// Sort the puck list by angle.  This is useful for the purposes of extracting meaning from
+	// the local puck configuration (e.g. fitting a lines to the detected pucks).
+	sort(puckList.pucks.begin(), puckList.pucks.end(), puckComparator);
+
+	puckListPublisher_ -> publish(puckList);
+
+	/* Get readings from proximity sensor */
+	const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+	ProximityList proxList;
+	proxList.n = tProxReads.size();
+	for (size_t i = 0; i < proxList.n; ++i) {
+		Proximity prox;
+		prox.value = tProxReads[i].Value;
+		prox.angle = tProxReads[i].Angle.GetValue();
+		proxList.proximities.push_back(prox);
+
+		//cout << GetId() << ": value: " << prox.value << ": angle: " << prox.angle << endl;
+	}
+
+	promixityPublisher_ -> publish(proxList);
+
+
+	// If we haven't heard from the subscriber in a while, set the speed to zero.
+	if (stepsSinceCallback > stopWithoutSubscriberCount) {
+		leftSpeed = 0;
+		rightSpeed = 0;
+	} else {
+		stepsSinceCallback++;
+	}
+
+	m_pcWheels->SetLinearVelocity(leftSpeed, rightSpeed);
+
+
+}
+void ArgosRosFootbot::cmdVelCallback(const geometry_msgs::msg::Twist& twist) {
+	//cout << "cmdVelCallback: " << GetId() << endl;
+
+	Real v = twist.linear.x;  // Forward speed
+	Real w = twist.angular.z; // Rotational speed
+
+	// Use the kinematics of a differential-drive robot to derive the left
+	// and right wheel speeds.
+	leftSpeed = (v - HALF_BASELINE * w) / WHEEL_RADIUS;
+	rightSpeed = (v + HALF_BASELINE * w) / WHEEL_RADIUS;
+
+	stepsSinceCallback = 0;
+}
+
+/*
+* This statement notifies ARGoS of the existence of the controller.
+* It binds the class passed as first argument to the string passed as
+* second argument.
+* The string is then usable in the configuration file to refer to this
+* controller.
+ * When ARGoS reads that string in the configuration file, it knows which
+ * controller class to instantiate.
+ * See also the configuration files for an example of how this is used.
+ */
+REGISTER_CONTROLLER(ArgosRosFootbot, "argos_ros_bot_controller")
--- plugins/argos_ros2_foot_bot/argos_ros_footbot.h
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.h
@@ -13,11 +13,113 @@
 
 #include <argos3/core/control_interface/ci_controller.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
-#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_colored_blob_omnidirectional_camera_sensor.h>
+#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
+
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+
+#include <string>
+#include <chrono>
+#include <memory>
+#include <iostream>
+#include <sstream>
+
+/**
+ * ROS2 Imports
+ */
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "argos_interface/msg/proximity_list.hpp"
+#include "argos_interface/msg/puck_list.hpp"
+
+using namespace argos;
+using namespace std::chrono_literals;
+
+
+class ArgosRosFootbot : public CCI_Controller{
+	private:
+		// Puck list publisher
+		rclcpp::Publisher<argos_interface::msg::PuckList>::SharedPtr puckListPublisher_;
+		// Proximity sensor publisher
+		rclcpp::Publisher<argos_interface::msg::ProximityList>::SharedPtr promixityPublisher_;
+		// Subscriber for cmd_vel (Twist message) topic.
+		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmdVelSubscriber_;
+
+		rclcpp::TimerBase::SharedPtr timer_;
+
+
+		CCI_DifferentialSteeringActuator* m_pcWheels;
+		CCI_ColoredBlobOmnidirectionalCameraSensor* m_pcOmniCam;
+		CCI_FootBotProximitySensor* m_pcProximity;
+
+		// The following constant values were copied from the argos source tree from
+		// the file src/plugins/robots/foot-bot/simulator/footbot_entity.cpp
+		static constexpr Real HALF_BASELINE = 0.07f; // Half the distance between wheels
+		static constexpr Real WHEEL_RADIUS = 0.029112741f;
+
+		/*
+		* The following variables are used as parameters for the
+		* algorithm. You can set their value in the <parameters> section
+		* of the XML configuration file, under the
+		* <controllers><argos_ros_bot_controller> section.
+		*/
+
+		// The number of time steps from the time step of the last callback
+		// after which leftSpeed and rightSpeed will be set to zero.  Useful to
+		// shutdown the robot after the controlling code on the ROS side has quit.
+		int stopWithoutSubscriberCount;
+
+		// The number of time steps since the last callback.
+		int stepsSinceCallback;
+
+		// Most recent left and right wheel speeds, converted from the ROS twist
+		// message.
+		Real leftSpeed, rightSpeed;
 
 
 
+	public:
+
+		ArgosRosFootbot();
+
+		virtual ~ArgosRosFootbot();
+
+		/*
+		* This function initializes the controller.
+		* The 't_node' variable points to the <parameters> section in the XML
+		* file in the <controllers><footbot_ccw_wander_controller> section.
+		*/
+		virtual void Init(TConfigurationNode& t_node);
+		/*
+		* This function is called once every time step.
+		* The length of the time step is set in the XML file.
+		*/
+		virtual void ControlStep();
+		/*
+		* This function resets the controller to its state right after the
+		* Init().
+		* It is called when you press the reset button in the GUI.
+		* In this example controller there is no need for resetting anything,
+		* so the function could have been omitted. It's here just for
+		* completeness.
+		*/
+		virtual void Reset(){}
+		/*
+		* Called to cleanup what done by Init() when the experiment finishes.
+		*/
+		virtual void Destroy() {}
+		/*
+		* The callback method for getting new commanded speed on the cmd_vel topic.
+		*/
+		void cmdVelCallback(const geometry_msgs::msg::Twist& twist);
+		
+		static std::shared_ptr<rclcpp::Node> nodeHandle;
+
+
+
+};
 
 
 
