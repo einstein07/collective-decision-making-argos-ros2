cfb5b491abd271111e98eea337be65091f319874
einstein07 <mkhsin035@myuct.ac.za>
2024-07-29 17:48:51 +0200

    Added communication - sharing commitment and robot-id

--- plugins/argos_ros2_foot_bot/argos_ros_footbot.cpp
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.cpp
@@ -9,8 +9,9 @@
 /* Include the controller definition */
 #include "argos_ros_footbot.h"
 using namespace std;
-using namespace argos_interface;
-using namespace argos_interface::msg;
+using namespace collective_decision_making;
+using namespace collective_decision_making::msg;
+using namespace geometry_msgs::msg;
 using std::placeholders::_1;
 
 /**
@@ -31,8 +32,13 @@ std::shared_ptr<rclcpp::Node> ArgosRosFootbot::nodeHandle = initNode();
 
 ArgosRosFootbot::ArgosRosFootbot() :
 		m_pcWheels(NULL),
-		m_pcOmniCam(NULL),
+		m_pcLight(NULL),
+		//m_pcLEDs(NULL),
+		m_pcCamera(NULL),
 		m_pcProximity(NULL),
+		m_pcPosition(NULL),
+		m_pcRABA(NULL),
+		m_pcRABS(NULL),
 		stopWithoutSubscriberCount(10),
 		stepsSinceCallback(0),
 		leftSpeed(0),
@@ -41,29 +47,57 @@ ArgosRosFootbot::ArgosRosFootbot() :
 ArgosRosFootbot::~ArgosRosFootbot(){}
 
 void ArgosRosFootbot::Init(TConfigurationNode& t_node){
-	// Create the topics to publish
-	stringstream puckListTopic, proximityTopic;
-	puckListTopic << "/puck_list";
-	proximityTopic << "/proximity";
-	puckListPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<argos_interface::msg::PuckList>(puckListTopic.str(), 1);
-	promixityPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<argos_interface::msg::ProximityList>(proximityTopic.str(), 1);
+	/********************************
+	 * Create the topics to publish
+	 *******************************/
+	stringstream lightTopic, blobTopic, proxTopic, positionTopic, rabTopic;
+	lightTopic 		<< "/" << GetId() << "/light";
+	blobTopic 		<< "/" << GetId() << "/blob";
+	proxTopic 		<< "/" << GetId() << "/proximity";
+	positionTopic 	<< "/" << GetId() << "/position";
+	rabTopic 		<< "/" << GetId() << "/rab";
+	lightListPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<LightList>(lightTopic.str(), 1);
+	blobPublisher_ 		= ArgosRosFootbot::nodeHandle -> create_publisher<BlobList>(blobTopic.str(), 1);
+	promixityPublisher_ = ArgosRosFootbot::nodeHandle -> create_publisher<ProximityList>(proxTopic.str(), 1);
+	positionPublisher_ 	= ArgosRosFootbot::nodeHandle -> create_publisher<Position>(positionTopic.str(), 1);
+	rabPublisher_ 		= ArgosRosFootbot::nodeHandle -> create_publisher<PacketList>(rabTopic.str(), 1);
 
-	// Create the subscriber
-	stringstream cmdVelTopic;
-	cmdVelTopic << "/cmd_vel";
-	cmdVelSubscriber_ = ArgosRosFootbot::nodeHandle -> create_subscription<geometry_msgs::msg::Twist>(
-			cmdVelTopic.str(),
-			1,
-			std::bind(&ArgosRosFootbot::cmdVelCallback, this, _1)
-			);
+	/*********************************
+	 * Create subscribers
+	 ********************************/
+	stringstream cmdVelTopic, cmdRabTopic;
+	cmdVelTopic 	<< "/" << GetId() << "/cmd_vel";
+	cmdRabTopic		<< "/" << GetId() << "/cmd_rab";
+	cmdVelSubscriber_ = ArgosRosFootbot::nodeHandle -> create_subscription<Twist>(
+						cmdVelTopic.str(),
+						1,
+						std::bind(&ArgosRosFootbot::cmdVelCallback, this, _1)
+						);
+	cmdRabSubscriber_ = ArgosRosFootbot::nodeHandle -> create_subscription<Packet>(
+						cmdRabTopic.str(),
+						1,
+						std::bind(&ArgosRosFootbot::cmdRabCallback, this, _1)
+						);
+	/********************************
+	 * Get sensor/actuator handles
+	 ********************************/
+	m_pcLight  		= GetSensor < CCI_FootBotLightSensor					>("footbot_light");
+	m_pcProximity 	= GetSensor < CCI_FootBotProximitySensor				>("footbot_proximity");
+	//m_pcLEDs   = GetActuator<CCI_LEDsActuator                          >("leds");
+	m_pcCamera 		= GetSensor < CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+	m_pcPosition 	= GetSensor < CCI_PositioningSensor						>("positioning");
+	m_pcRABS 		= GetSensor < CCI_RangeAndBearingSensor					>("range_and_bearing" );
 
-	// Get sensor/actuator handles
-	m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
-	m_pcProximity = GetSensor<CCI_FootBotProximitySensor>("footbot_proximity");
-	m_pcOmniCam = GetSensor<CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+	/********************************
+	 * Get actuator handles
+	 ********************************/
+	m_pcWheels = GetActuator< CCI_DifferentialSteeringActuator >("differential_steering");
+	m_pcRABA = GetActuator< CCI_RangeAndBearingActuator        >("range_and_bearing" );
 
-	m_pcOmniCam->Enable();
-
+	/*
+	* Other init stuff
+	*/
+	Reset();
 	/*
 	* Parse the configuration file
 	*
@@ -73,36 +107,36 @@ void ArgosRosFootbot::Init(TConfigurationNode& t_node){
 	*/
 	GetNodeAttributeOrDefault(t_node, "stopWithoutSubscriberCount", stopWithoutSubscriberCount, stopWithoutSubscriberCount);
 
-
 }
 
-bool puckComparator(Puck a, Puck b) {
+bool blobComparator(Blob a, Blob b) {
 	return a.angle < b.angle;
 }
 
 void ArgosRosFootbot::ControlStep() {
+
 	rclcpp::spin_some(ArgosRosFootbot::nodeHandle);
-	const CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings& camReads = m_pcOmniCam->GetReadings();
-	PuckList puckList;
-	puckList.n = camReads.BlobList.size();
-	for (size_t i = 0; i < puckList.n; ++i) {
-		Puck puck;
-		puck.type = (camReads.BlobList[i]->Color == CColor::RED);
-		puck.range = camReads.BlobList[i]->Distance;
-		// Make the angle of the puck in the range [-PI, PI].  This is useful for
-		// tasks such as homing in on a puck using a simple controller based on
-		// the sign of this angle.
-		puck.angle = camReads.BlobList[i]->Angle.SignedNormalize().GetValue();
-		puckList.pucks.push_back(puck);
+
+	/*********************************
+	 * Get readings from light sensor
+	 *********************************/
+	const CCI_FootBotLightSensor::TReadings& tLightReads = m_pcLight->GetReadings();
+	LightList lightList;
+	lightList.n = tLightReads.size();
+	for (size_t i = 0; i < lightList.n; ++i) {
+		Light light;
+		light.value = tLightReads[i].Value;
+		light.angle = tLightReads[i].Angle.GetValue();
+		lightList.lights.push_back(light);
+
+		//cout << GetId() << ": value: " << light.value << ": angle: " << light.angle << endl;
 	}
 
-	// Sort the puck list by angle.  This is useful for the purposes of extracting meaning from
-	// the local puck configuration (e.g. fitting a lines to the detected pucks).
-	sort(puckList.pucks.begin(), puckList.pucks.end(), puckComparator);
+	lightListPublisher_ -> publish(lightList);
 
-	puckListPublisher_ -> publish(puckList);
-
-	/* Get readings from proximity sensor */
+	/***********************************
+	 * Get readings from proximity sensor
+	 ***********************************/
 	const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
 	ProximityList proxList;
 	proxList.n = tProxReads.size();
@@ -117,6 +151,133 @@ void ArgosRosFootbot::ControlStep() {
 
 	promixityPublisher_ -> publish(proxList);
 
+	/**************************************************************
+	 * Get readings from Colored Blob Omnidirectional Camera Sensor
+	 *************************************************************/
+	const CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings& camReads = m_pcCamera->GetReadings();
+	BlobList blobList;
+	blobList.n = camReads.BlobList.size();
+	Blob blob;
+	for (size_t i = 0; i < blobList.n; ++i) {
+		//Blob blob;
+		stringstream ss;
+		ss << camReads.BlobList[i]->Color;
+		blob.color = ss.str();
+		blob.distance = camReads.BlobList[i]->Distance;
+
+		// Make the angle of the puck in the range [-PI, PI].  This is useful for
+		// tasks such as homing in on a puck using a simple controller based on
+		// the sign of this angle.
+		blob.angle = camReads.BlobList[i]->Angle.GetValue();//.SignedNormalize().GetValue();
+		blobList.blobs.push_back(blob);
+		//std::cout << "value: " << blob.distance << ": angle: " << blob.angle << " color: " << blob.color << std::endl;
+
+	}
+
+	// Sort the puck list by angle.  This is useful for the purposes of extracting meaning from
+	// the local puck configuration (e.g. fitting a lines to the detected pucks).
+	sort(blobList.blobs.begin(), blobList.blobs.end(), blobComparator);
+
+	blobPublisher_ -> publish(blobList);
+
+	/*********************************************************************
+	 * Get readings from Positioning sensor
+	 * TODO: Find an elegant way to make assignment
+	 * Problem: can't directly assign argos::CVector3 to geometry::Vector3
+	 * Same with the Quaternion
+	 **********************************************************************/
+	const CCI_PositioningSensor::SReading& tPosReads = m_pcPosition->GetReading();
+	Position position;
+
+	position.position.x = tPosReads.Position.GetX();
+	position.position.y = tPosReads.Position.GetY();
+	position.position.z = tPosReads.Position.GetZ();
+
+	position.orientation.w = tPosReads.Orientation.GetW();
+	position.orientation.x = tPosReads.Orientation.GetX();
+	position.orientation.y = tPosReads.Orientation.GetY();
+	position.orientation.z = tPosReads.Orientation.GetZ();
+
+	positionPublisher_ -> publish(position);
+
+	/**CVector3 pos(
+				position.position.x,
+				position.position.y,
+				position.position.z
+				);
+	float r = float(blob.distance/100.0f);
+	float x = r * cos(blob.angle);
+	float y = r * sin(blob.angle);
+	float dist = std::sqrt(std::pow((2 - pos.GetX()), 2) + std::pow((18 - pos.GetY()), 2));
+
+	std::cout 	<< "calculated distance: " << dist << " measured in cm: "
+				<< blob.distance << " in meters: "
+				<< float(blob.distance/100.0f)
+				<< " angle: " << blob.angle << std::endl;
+
+	CVector2 toTarget(float(pos.GetX() + x), float(pos.GetY() + y));
+	CQuaternion toTarget3(0, x, y, 0);
+	CQuaternion prod1 = toTarget3 * tPosReads.Orientation;
+	prod1 = prod1 * tPosReads.Orientation.Conjugate();
+	float x_g = prod1.GetX() + tPosReads.Position.GetX();
+	float y_g = prod1.GetY() + tPosReads.Position.GetY();
+	std::cout 	<< "relative target- x: " << x << " y: " << y
+				<< " current position: " << pos
+				<< " Orientation: " << tPosReads.Orientation << std::endl;
+	std::cout << "global target: " << x_g << ", " << y_g << std::endl;*/
+
+	/*********************************************
+	 * Get readings from Range-And-Bearing-Sensor
+	 *********************************************/
+	const CCI_RangeAndBearingSensor::TReadings& tRabReads = m_pcRABS->GetReadings();
+	PacketList packetList;
+	packetList.n = tRabReads.size();
+	for (size_t i = 0; i < packetList.n; ++i) {
+		Packet packet;
+		packet.range = tRabReads[i].Range;
+		packet.h_bearing = tRabReads[i].HorizontalBearing.GetValue();
+		packet.v_bearing = tRabReads[i].VerticalBearing.GetValue();
+
+		// Read out coordinates, keeping tract of signs
+		/**float x = tRabReads[i].Data[0];
+		float y = tRabReads[i].Data[1];
+		float id = tRabReads[i].Data[4];
+
+		x = (tRabReads[i].Data[2] > 0) ? (-x) : x;
+		y = (tRabReads[i].Data[3] > 0) ? (-y) : y;
+
+
+		packet.data.push_back(x);
+		packet.data.push_back(y);*/
+		if (tRabReads[i].Data[0] > 0){
+			cout << GetId() << ": light-source-id: " << tRabReads[i].Data[0] << endl;
+			packet.data.push_back(tRabReads[i].Data[0]);
+			packet.data.push_back(tRabReads[i].Data[1]);
+
+			packetList.packets.push_back(packet);
+		}
+		//cout << GetId() << ": id: " << tRabReads[i].Data[1] << endl;
+	}
+
+	rabPublisher_ -> publish(packetList);
+	m_pcRABA -> ClearData();
+	/* Get the camera readings */
+	/**const CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings& sReadings = m_pcCamera->GetReadings();
+	BlobList blobList;
+	blobList.n = sReadings.BlobList.size();
+	for (size_t i = 0; i < blobList.n; ++i) {
+		Blob blob;
+		blob.distance = sReadings.BlobList[i]->Distance;
+		blob.angle = sReadings.BlobList[i]->Angle.GetValue();
+		blob.color = (sReadings.BlobList[i]->Color == CColor::RED)? "red" : "";
+
+		blobList.blobs.push_back(blob);
+
+		//cout << GetId() << ": value: " << prox.value << ": angle: " << prox.angle << endl;
+	}
+
+	blobPublisher_ -> publish(blobList);*/
+
 
 	// If we haven't heard from the subscriber in a while, set the speed to zero.
 	if (stepsSinceCallback > stopWithoutSubscriberCount) {
@@ -125,12 +286,20 @@ void ArgosRosFootbot::ControlStep() {
 	} else {
 		stepsSinceCallback++;
 	}
-
+	//std::cout << GetId() << ": left-wheel: " << leftSpeed << ": right-wheel: " << rightSpeed << std::endl;
 	m_pcWheels->SetLinearVelocity(leftSpeed, rightSpeed);
 
 
 }
-void ArgosRosFootbot::cmdVelCallback(const geometry_msgs::msg::Twist& twist) {
+
+void ArgosRosFootbot::Reset() {
+   /* Enable camera filtering */
+   m_pcCamera->Enable();
+   /* Set beacon color to all red to be visible for other robots */
+   /*m_pcLEDs->SetSingleColor(12, CColor::RED);*/
+}
+
+void ArgosRosFootbot::cmdVelCallback(const Twist& twist) {
 	//cout << "cmdVelCallback: " << GetId() << endl;
 
 	Real v = twist.linear.x;  // Forward speed
@@ -138,10 +307,55 @@ void ArgosRosFootbot::cmdVelCallback(const geometry_msgs::msg::Twist& twist) {
 
 	// Use the kinematics of a differential-drive robot to derive the left
 	// and right wheel speeds.
-	leftSpeed = (v - HALF_BASELINE * w) / WHEEL_RADIUS;
-	rightSpeed = (v + HALF_BASELINE * w) / WHEEL_RADIUS;
+	if (twist.linear.z == 1.0){
+		leftSpeed = (v - HALF_BASELINE * w) / WHEEL_RADIUS;
+		rightSpeed = (v + HALF_BASELINE * w) / WHEEL_RADIUS;
+	}
+	else{ // Drive to target
+		leftSpeed = twist.linear.x;
+		rightSpeed = twist.linear.y;
+	}
+
+
 
 	stepsSinceCallback = 0;
+}
+
+void ArgosRosFootbot::cmdRabCallback(const Packet& packet){
+	cout << GetId() << " Packet data as received: " << packet.data[0] <<endl;
+	//m_pcRABA -> SetData(0, 1); // validity flag
+	m_pcRABA -> SetData(0, packet.data[0]);
+	m_pcRABA -> SetData(1, std::stoi( packet.id ));
+
+	/**if (isSigned(packet.data[0])){
+		m_pcRABA -> SetData(0, -packet.data[0]);
+		m_pcRABA -> SetData(2, 1);
+	}
+	else{
+		m_pcRABA -> SetData(0, packet.data[0]);
+		m_pcRABA -> SetData(2, 0);
+	}
+	if (isSigned(packet.data[1])){
+		m_pcRABA -> SetData(1, -packet.data[1]);
+		m_pcRABA -> SetData(3, 1);
+	}
+	else{
+		m_pcRABA -> SetData(3, 0);
+		m_pcRABA -> SetData(1, packet.data[1]);
+	}
+	if (isSigned(packet.data[2])){
+		m_pcRABA -> SetData(4, -packet.data[2]);
+		m_pcRABA -> SetData(5, 1);
+	}
+	else{
+		m_pcRABA -> SetData(4, packet.data[2]);
+		m_pcRABA -> SetData(5, 0);
+	}*/
+}
+
+bool ArgosRosFootbot::isSigned(float num)
+{
+	return num<0; //std::is_signed<decltype(num)>::value ;
 }
 
 /*
--- plugins/argos_ros2_foot_bot/argos_ros_footbot.h
+++ plugins/argos_ros2_foot_bot/argos_ros_footbot.h
@@ -10,11 +10,25 @@
 
 #ifndef ARGOS_ROS_FOOTBOT_H_
 #define ARGOS_ROS_FOOTBOT_H_
-
+/* Definition of the CCI_Controller class. */
 #include <argos3/core/control_interface/ci_controller.h>
+/* Definition of the differential steering actuator */
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+/* Definition of the LEDs actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_leds_actuator.h>
+/* Definition of the omnidirectional camera sensor */
 #include <argos3/plugins/robots/generic/control_interface/ci_colored_blob_omnidirectional_camera_sensor.h>
+/* Definition of the range-and-bearing actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
+/* Definition of the range-and-bearing sensor */
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
+/* Definition of the foot-bot light sensor */
+#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_light_sensor.h>
+/* Definition of the foot-bot proximity sensor */
 #include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
+/* Definition of the positioning sensor */
+#include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
+
 
 /* Function definitions for XML parsing */
 #include <argos3/core/utility/configuration/argos_configuration.h>
@@ -24,6 +38,7 @@
 #include <memory>
 #include <iostream>
 #include <sstream>
+#include <cuchar>
 
 /**
  * ROS2 Imports
@@ -31,8 +46,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/bool.hpp"
-#include "argos_interface/msg/proximity_list.hpp"
-#include "argos_interface/msg/puck_list.hpp"
+#include "collective_decision_making/msg/light_list.hpp"
+#include "collective_decision_making/msg/blob_list.hpp"
+#include "collective_decision_making/msg/proximity_list.hpp"
+#include "collective_decision_making/msg/position.hpp"
+#include "collective_decision_making/msg/packet.hpp"
+#include "collective_decision_making/msg/packet_list.hpp"
 
 using namespace argos;
 using namespace std::chrono_literals;
@@ -40,19 +59,45 @@ using namespace std::chrono_literals;
 
 class ArgosRosFootbot : public CCI_Controller{
 	private:
-		// Puck list publisher
-		rclcpp::Publisher<argos_interface::msg::PuckList>::SharedPtr puckListPublisher_;
+		// Light list publisher
+		rclcpp::Publisher<collective_decision_making::msg::LightList>::SharedPtr lightListPublisher_;
 		// Proximity sensor publisher
-		rclcpp::Publisher<argos_interface::msg::ProximityList>::SharedPtr promixityPublisher_;
+		rclcpp::Publisher<collective_decision_making::msg::ProximityList>::SharedPtr promixityPublisher_;
+
+		rclcpp::Publisher<collective_decision_making::msg::BlobList>::SharedPtr blobPublisher_;
+		// Position sensor publisher
+		rclcpp::Publisher<collective_decision_making::msg::Position>::SharedPtr positionPublisher_;
+		// Position sensor publisher
+		rclcpp::Publisher<collective_decision_making::msg::PacketList>::SharedPtr rabPublisher_;
+
+		/************************************
+		 * Subscribers
+		 ***********************************/
 		// Subscriber for cmd_vel (Twist message) topic.
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmdVelSubscriber_;
+		// Subscriber for cmd_rab (Packet) topic
+		rclcpp::Subscription<collective_decision_making::msg::Packet>::SharedPtr cmdRabSubscriber_;
 
 		rclcpp::TimerBase::SharedPtr timer_;
 
 
+		/* Pointer to the differential steering actuator */
 		CCI_DifferentialSteeringActuator* m_pcWheels;
-		CCI_ColoredBlobOmnidirectionalCameraSensor* m_pcOmniCam;
+		/* Pointer to the foot-bot light sensor */
+		CCI_FootBotLightSensor* m_pcLight;
+		/* Pointer to the LEDs actuator */
+		//CCI_LEDsActuator* m_pcLEDs;
+		/* Pointer to the omnidirectional camera sensor */
+		CCI_ColoredBlobOmnidirectionalCameraSensor* m_pcCamera;
+		/* Pointer to proximity sensor */
 		CCI_FootBotProximitySensor* m_pcProximity;
+		/* Pointer to positioning sensor */
+		CCI_PositioningSensor* m_pcPosition;
+		/* Pointer to the range-and-bearing sensor */
+		CCI_RangeAndBearingSensor* m_pcRABS;
+		/* Pointer to the range-and-bearing actuator */
+		CCI_RangeAndBearingActuator* m_pcRABA;
+
 
 		// The following constant values were copied from the argos source tree from
 		// the file src/plugins/robots/foot-bot/simulator/footbot_entity.cpp
@@ -105,7 +150,7 @@ class ArgosRosFootbot : public CCI_Controller{
 		* so the function could have been omitted. It's here just for
 		* completeness.
 		*/
-		virtual void Reset(){}
+		virtual void Reset();
 		/*
 		* Called to cleanup what done by Init() when the experiment finishes.
 		*/
@@ -114,7 +159,14 @@ class ArgosRosFootbot : public CCI_Controller{
 		* The callback method for getting new commanded speed on the cmd_vel topic.
 		*/
 		void cmdVelCallback(const geometry_msgs::msg::Twist& twist);
-		
+		/*
+		 * The callback method for getting new commanded packet on the cmd_packet topic.
+		 */
+		void cmdRabCallback(const collective_decision_making::msg::Packet& packet);
+
+		static bool isSigned(float num);
+
+
 		static std::shared_ptr<rclcpp::Node> nodeHandle;
 
 
